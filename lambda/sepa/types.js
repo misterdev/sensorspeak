3e7f4348ca6330b9391deba47ead1f055933a8e3
Devid Farinelli <devid.farinelli@gmail.com>
2019-02-02 18:11:49 +0100

    sorry travis, i messed up smting in the gitignore man

---
+++ lambda/sepa/types.js
@@ -0,0 +1,11 @@
+module.exports = {
+  temperature: 'http://purl.org/iot/vocab/m3-lite#Temperature',
+  'board temperature': 'http://purl.org/iot/vocab/m3-lite#BoardTemperature',
+  'building temperature':
+    'http://purl.org/iot/vocab/m3-lite#BuildingTemperature',
+  humidity: 'http://purl.org/iot/vocab/m3-lite#Humidity',
+  'soil humidity': 'http://purl.org/iot/vocab/m3-lite#SoilHumidity',
+  'air pressure': 'http://purl.org/iot/vocab/m3-lite#AtmosphericPressure',
+  voltage: 'http://purl.org/iot/vocab/m3-lite#Voltage',
+  'battery level': 'http://purl.org/iot/vocab/m3-lite#BatteryLevel'
+}
