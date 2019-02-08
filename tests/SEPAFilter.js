c05d53687707538a7cff6ecbbc5bec7467f5fbcf
Devid Farinelli <devid.farinelli@gmail.com>
2019-02-08 16:15:20 +0100

    Travis take this

--- tests/SEPAFilter.js
+++ tests/SEPAFilter.js
@@ -25,6 +25,16 @@ function LaunchRequestFilter () {
         return simpleResponse('Italy-Site1-Pressure-label')
       else return 'error'
     })
+  nock('http://mml.arces.unibo.it:8000')
+    .persist()
+    .get('/query')
+    .reply(200, (uri, requestBody) => {
+      const query = queryString.parse(requestBody).query
+      const expectedQuery = SEPA.LaunchRequestQuery()
+      if (query.indexOf(expectedQuery) > 0)
+        return simpleResponse('Italy-Site1-Pressure-label')
+      else return 'error'
+    })
 }
 
 const simpleResponse = value => ({
